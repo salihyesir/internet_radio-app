32bd2620116e26d696f653cc33192adc222b26ca
Salih Yesir <salih.yesir@ogr.sakarya.edu.tr>
2017-11-17 12:40:01 +0300

    Volumebar ayarlandı
    Görüntü iyileştirildi. Volemebar özellik kazandırıldı.

--- app/src/main/java/com/internetradio/bt/proje/MainActivity.java
+++ app/src/main/java/com/internetradio/bt/proje/MainActivity.java
@@ -1,13 +1,17 @@
 package com.internetradio.bt.proje;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -16,40 +20,66 @@ import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton b_mainplaybutton;
 
-    Button b_channelOn;
 
-    int[] IMAGES={R.drawable.alem,R.drawable.ntv,R.drawable.show,R.drawable.superfm,R.drawable.kralfm,R.drawable.trafik,
-            R.drawable.power,R.drawable.mydonose,R.drawable.trt,R.drawable.virgin};
+    int[] IMAGES = {R.drawable.alem, R.drawable.ntv, R.drawable.show, R.drawable.superfm, R.drawable.kralfm, R.drawable.trafik,
+            R.drawable.power, R.drawable.mydonose, R.drawable.trt, R.drawable.virgin};
 
-    String[] NAMES={"Alem FM","NTV Radyo","ShowFM","SuperFM","KralFM","TrafikFM","PowerFM",
-                "MydonoseFM","TrtFM","VirginRadyo"};
+    String[] NAMES = {"Alem FM", "NTV Radyo", "ShowFM", "SuperFM", "KralFM", "TrafikFM", "PowerFM",
+            "MydonoseFM", "TrtFM", "VirginRadyo"};
 
-    String[] DESCRIPTIONS={"deneme1","deneme2","deneme3","deneme4","deneme5","deneme6","deneme7","deneme8"
-            ,"deneme9","deneme10"};
+    String[] DESCRIPTIONS = {"deneme1", "deneme2", "deneme3", "deneme4", "deneme5", "deneme6", "deneme7", "deneme8"
+            , "deneme9", "deneme10"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b_channelOn = (Button)findViewById(R.id.b_channelOn);
 
-        //Butonumuza tıklama özelliği kazandırıyoruz.
-        b_channelOn.setOnClickListener(new View.OnClickListener() {
+        ListView listView = (ListView) findViewById(R.id.listView);
 
-            @Override
-            public void onClick(View v) {
-                //Ardından Intent methodunu kullanarak nereden nereye gideceğini söylüyoruz.
-                Intent intocan = new Intent(MainActivity.this, Radio.class);
-                startActivity(intocan);
-            }
-        });
-
-        ListView listView=(ListView)findViewById(R.id.listView);
-
-        CustomAdapter customAdapter=new CustomAdapter();
+        CustomAdapter customAdapter = new CustomAdapter();
 
         listView.setAdapter(customAdapter);
+        b_mainplaybutton = (ImageButton) findViewById(R.id.mainplaybutton);
+        b_mainplaybutton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intocan = new Intent(MainActivity.this, Radio.class);
+                    startActivity(intocan);
+                }
+        });
+/*
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //Intent intocan = new Intent(MainActivity.this, Radio.class);
+                //startActivity(intocan);
+
+
+
+                AlertDialog.Builder diyalogOlusturucu =
+                        new AlertDialog.Builder(MainActivity.this);
+
+                diyalogOlusturucu.setMessage(NAMES[position])
+                        .setCancelable(false)
+                        .setPositiveButton("Tamam", new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                diyalogOlusturucu.create().show();
+
+            }
+
+        });
+  */
     }
 
 
--- app/src/main/java/com/internetradio/bt/proje/Radio.java
+++ app/src/main/java/com/internetradio/bt/proje/Radio.java
@@ -3,21 +3,38 @@ package com.internetradio.bt.proje;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.AsyncTask;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
 
 import java.io.IOException;
 
 public class Radio extends AppCompatActivity {
 
-    Button b_radioOn;
+    private String streamUrl = "http://sc.powergroup.com.tr/RadyoFenomen/mpeg/128/tunein";
+    private ImageButton startBtn;
+    private ImageButton stopBtn;
+    private MediaPlayer player;
+    public static boolean isAlreadyPlaying = false;
+    private AudioManager audioManager;
+    private SeekBar volumeBar;
 
-    MediaPlayer mediaPlayer;
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-    boolean prepared = false;
-    boolean started = false;
+        if(isAlreadyPlaying){
+
+            playRadioPlayer();
+        }else{
+            stopRadioPlayer();
+        }
+    }
 
     String stream="http://streams.fluxfm.de/klubradio/mp3-320";
     @Override
@@ -25,73 +42,103 @@ public class Radio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_radio);
 
-        b_radioOn = (Button) findViewById(R.id.b_radioOn);
-        b_radioOn.setEnabled(false);
-        b_radioOn.setText(" _ ");
-        mediaPlayer = new MediaPlayer();
-        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        audioManager = (AudioManager)getSystemService(getApplicationContext().AUDIO_SERVICE);
 
-        new PlayerTask().execute(stream);
+        volumeBar = (SeekBar) findViewById(R.id.radioseekBar2) ;
+        volumeBar.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_RING));
+        volumeBar.setKeyProgressIncrement(10);
+        volumeBar.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_RING));
 
-        b_radioOn.setOnClickListener(new View.OnClickListener() {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); // uyku modunu kapatma
+
+        startBtn = (ImageButton) findViewById(R.id.radioplaybutton);
+        stopBtn = (ImageButton) findViewById(R.id.radiostopbutton);
+        stopBtn.setEnabled(false);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (started){
-                    mediaPlayer.start();
-                    b_radioOn.setText(" p ");
-                }
-                else{
-                    started=true;
-                    mediaPlayer.start();
-                    b_radioOn.setText("pause");
-                }
+
+
+                isAlreadyPlaying = true;
+                playRadioPlayer();
+
+
+            }
+        });
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                isAlreadyPlaying = false;
+                stopRadioPlayer();
+
+            }
+        });
+        initializeMediaPlayer();
+
+        volumeBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
 
     }
-    class PlayerTask extends AsyncTask<String, Void, Boolean> {
 
-        @Override
-        protected Boolean doInBackground(String... strings)
-        {
-            try {
+    private void playRadioPlayer() {
 
-                mediaPlayer.setDataSource(strings[0]);
-                mediaPlayer.prepare();
-                prepared=true;
-            }catch (IOException e) {
-                e.printStackTrace();
+        stopBtn.setEnabled(true);
+        startBtn.setEnabled(false);
+        player.prepareAsync();
+        player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+
+            public void onPrepared(MediaPlayer mp) {
+                player.start();
             }
-            return prepared;
+        });
+    }
+
+    private void stopRadioPlayer() {
+
+        if (player.isPlaying()) {
+            player.stop();
+            player.release();
+            initializeMediaPlayer();
         }
 
-        @Override
-        protected void onPostExecute(Boolean aBoolean){
-            super.onPostExecute(aBoolean);
+        startBtn.setEnabled(true);
+        stopBtn.setEnabled(false);
+    }
 
-            b_radioOn.setEnabled(true);
-            b_radioOn.setText(" Playing ");
+    private void initializeMediaPlayer() {
+        player = new MediaPlayer();
+        try {
+            player.setDataSource(streamUrl);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (started)
-            mediaPlayer.pause();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (started)
-            mediaPlayer.start();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (prepared)
-            mediaPlayer.release();
+        if (player.isPlaying()) {
+            player.stop();
+        }
     }
 }
